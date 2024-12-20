378d7f9492ed75c3a3a51dc8a3aec34970bf8ac4
PAPASHKA1OR <75623978+PAPASHKA1OR@users.noreply.github.com>
2024-12-20 22:08:25 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <img style="display: block;-webkit-user-select: none;margin: auto;cursor: zoom-in;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src="storms.jpg" width="781" height="781">
+  <script src="script.js"></script>
+</body>
+
+</html>
